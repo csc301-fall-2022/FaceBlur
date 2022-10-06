9ef59ddbf2554106f0e1a1007db007c3860ec9af
Siddarth Dagar <sidmcheath@gmail.com>
2022-10-05 21:26:20 -0400

    Dev config (#4)
    * typescript dependedncies + top level git ignore
    
    * tsc command and react type definitions
    
    * build is tsc and multiple gitignore added back
    
    * dev mode and dev dependencies added
    
    * changed target to es6, no polyfill
    
    * redid frontend from scratch
    
    * documentation
    
    * lint fix

---
+++ frontend/webpack.config.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import webpack, {Configuration} from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+import packageJSON = require("./package.json");
+import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
+
+const webpackConfig = (env): Configuration => ({
+    entry: "./src/index.tsx",
+    ...(env.production || !env.development ? {} : {devtool: "eval-source-map"}),
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        plugins: [new TsconfigPathsPlugin()]
+    },
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                options: {
+                    transpileOnly: true
+                },
+                exclude: /dist/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new webpack.DefinePlugin({
+            "process.env.PRODUCTION": env.production || !env.development,
+            "process.env.NAME": JSON.stringify(packageJSON.name),
+            "process.env.VERSION": JSON.stringify(packageJSON.version)
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+        new ESLintPlugin({files: "./src/**/*.{ts,tsx,js,jsx}"})
+    ]
+});
+
+export default webpackConfig;
