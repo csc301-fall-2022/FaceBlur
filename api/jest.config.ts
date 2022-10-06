aab3ec712766047fea8af47e38e86fc27940bd88
Baker Jackson <bakerwjackson@gmail.com>
2022-10-05 23:09:05 -0400

    add README and logger

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -192,4 +192,4 @@ export default {
 
     // Whether to use watchman for file crawling
     // watchman: true,
-}
+};
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { logger } from './utils/logger';
 
 dotenv.config();
 
@@ -11,5 +12,5 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server started at port ${port}`);
+    logger.info(`Server started at port ${port}`);
 });
