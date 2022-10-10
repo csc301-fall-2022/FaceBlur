7c4dec0361b680feb6c1f01be7941fa18eab5cc0
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2022-10-10 00:39:16 -0400

    file upload with multer

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,39 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import multer from 'multer';
+import { storage, fileFilter } from './middleware/upload'
+
 import { logger } from './utils/logger';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+// TODO: maybe we need a filesize limit 
+const upload = multer({storage: storage, fileFilter: fileFilter}).single('file');
+
+app.use(express.json());
+
+// Endpoint for uploading and processing a video
+app.post('/upload', (req: Request, res: Response) => {
+    logger.info('Upload endpoint called');
+    // TODO: need to get blur type, then process video in the appropriate function 
+    // Upload file with multer
+    upload(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            logger.error('Error uploading file: Multer error');
+            res.status(400).send(err.message);
+        } else if (err) {
+            logger.error('Error uploading file: Unknown error');
+            res.status(400).send(err.message);
+        } else {
+            // TODO: populate schema with file metadata and save to local db
+            logger.info('File uploaded successfully');
+            res.status(200).send(req.file);
+        }
+    });
+});
 
 app.get('/', (req: Request, res: Response) => {
     res.send('CSC311 Express Server');
---
+++ api/src/middleware/upload.ts
@@ -0,0 +1,28 @@
+import multer, { FileFilterCallback, StorageEngine } from 'multer';
+import { Request } from 'express';
+import path from 'path';
+
+
+// Save to local storage: tentative solution
+export const storage: StorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname); 
+    }
+});
+
+// Only allow video files to be uploaded
+export const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
+    const fileTypes = /mp4|mov|avi|wmv|flv|mkv/;
+
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = fileTypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb(new Error('Error: Video files only'));
+    }
+}
