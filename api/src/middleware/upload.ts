a0b9fc0e5cad1b385417b53c027eed30f95d57d0
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2022-11-02 19:10:21 -0400

    multer s3

--- api/src/middleware/upload.ts
+++ api/src/middleware/upload.ts
@@ -1,11 +1,34 @@
 import multer, { FileFilterCallback, StorageEngine } from 'multer';
 import { Request } from 'express';
 import path from 'path';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
+
+// init s3 client
+const s3 = new aws.S3(
+    {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+        region: process.env.AWS_BUCKET_REGION
+    }
+);
 
 // TODO: Need to save videos to S3 
-export const storage: StorageEngine = multer.diskStorage({
-    filename: (req, file, cb) => {
-        cb(null, file.originalname); 
+// export const storage: StorageEngine = multer.diskStorage({
+//     filename: (req, file, cb) => {
+//         cb(null, file.originalname); 
+//     }
+// });
+
+export const storage: StorageEngine = multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+        cb(null, { fieldName: "TESTING_METADATA" });
+    },
+    key: (req, file, cb) => {
+        cb(null, file.originalname);
     }
 });
 
