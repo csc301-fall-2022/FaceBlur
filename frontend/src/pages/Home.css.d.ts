a3051c02dd837f00ea991cb9e9a1ca3715dded7b
Kenneth Miura <kenneth.t.miura@gmail.com>
2022-11-06 12:04:02 -0500

    added overrides for upload css

--- frontend/src/pages/Home.css.d.ts
+++
@@ -1,6 +0,0 @@
-export const searchbar: string;
-export const homepage-container: string;
-export const display-container: string;
-export const css-lntp72-MuiButtonBase-root-MuiFab-root: string;
-export const uploadButton: string;
-export const MuiTableRow-root: string;
