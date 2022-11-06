1f9b63c41a2ee43c5bc7a02e6615ae95b1bfd4e4
Baker Jackson <bakerwjackson@gmail.com>
2022-11-06 10:31:53 -0500

    fix tests, user registration, and backend dependencies

--- api/test/auth.test.ts
+++ api/test/auth.test.ts
@@ -4,69 +4,62 @@ import app from '../src/app';
 import { logger } from '../src/utils/logger';
 
 describe('registration success', () => {
-  test('register a user', async() => {
-    const user = {
-        id: 1,
-        email: "test@gmail.com",
-        password: "$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52"
-    };
-    try {
-        const res = await request(app).post("/api/auth/register").send(user);
+    test('register a user', async () => {
+        const user = {
+            id: 1,
+            email: 'test@gmail.com',
+            password:
+                '$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52',
+        };
+        const res = await request(app).post('/api/auth/register').send(user);
         // response check
-        expect(res.body).toEqual({status: "succeeded"});
+        expect(res.body).toEqual({ status: 'succeeded' });
         expect(res.statusCode).toBe(200);
         return res;
-    } catch (err) {
-        logger.error(err);
-    }
-  });
+    });
 });
 
 describe('registration fail', () => {
-  test('duplicate users', async() => {
-    const user1 = {
-        id: 1,
-        email: "test@gmail.com",
-        password: "$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52"
-    };
-    const user2 = {
-        id: 2,
-        email: "test@gmail.com",
-        password: "$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52"
-    };
-    try {
-        await request(app).post("/api/auth/register").send(user1);
-        const res = await request(app).post("/api/auth/register").send(user2);
+    test('duplicate users', async () => {
+        const user1 = {
+            id: 1,
+            email: 'test@gmail.com',
+            password:
+                '$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52',
+        };
+        const user2 = {
+            id: 2,
+            email: 'test@gmail.com',
+            password:
+                '$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52',
+        };
+        await request(app).post('/api/auth/register').send(user1);
+        const res = await request(app).post('/api/auth/register').send(user2);
         // response check
-        expect(res.body).toEqual({"message": "Email In Use", status: "failed"});
+        expect(res.body).toEqual({ message: 'Email In Use', status: 'failed' });
         expect(res.statusCode).toBe(500);
         return res;
-    } catch (err) {
-        logger.error(err);
-    }
-  });
+    });
 });
 
 describe('login success', () => {
-  test('login a user', async() => {
-    const userReg = {
-        id: 1,
-        email: "test@gmail.com",
-        password: "$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52"
-    };
-    const user = {
-        email: "test@gmail.com",
-        password: "$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52"
-    };
-    try {
-        await request(app).post("/api/auth/register").send(userReg);
-        const res = await request(app).post("/api/auth/login").send(user);
+    test('login a user', async () => {
+        const userReg = {
+            id: 1,
+            email: 'test@gmail.com',
+            password:
+                '$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52',
+        };
+        const user = {
+            email: 'test@gmail.com',
+            password:
+                '$2b$10$UNQGu4eZmEgvucwIIC/DMeszMuvHXf/6euvCwUYAlS2YxRm0WLC52',
+        };
+        await request(app).post('/api/auth/register').send(userReg);
+        const res = await request(app).post('/api/auth/login').send(user);
         // response check
-        expect(res.body).toEqual({status: "succeeded"});
+        expect(res.body).toEqual({ status: 'success' });
         expect(res.statusCode).toBe(200);
         return res;
-    } catch (err) {
-        logger.error(err);
-    }
-  });
+    });
 });
