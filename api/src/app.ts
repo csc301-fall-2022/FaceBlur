81b5e63e11960b47260dfa10f01ccf8fbb3786ef
Baker Jackson <bakerwjackson@gmail.com>
2022-11-06 23:50:20 -0500

    setup app for heroku deploy

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response } from 'express';
-import video_list from './routes/video_list'
+import video_list from './routes/video_list';
 import auth from './routes/auth';
 import dotenv from 'dotenv';
 import passport from 'passport';
@@ -19,6 +19,10 @@ app.use('/api/auth', auth);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('../frontend/dist'));
+}
+
 // upload route
 app.use(
     '/api/upload',
@@ -26,16 +30,16 @@ app.use(
     upload
 );
 
-app.use("/api/video_list", video_list);
+app.use('/api/video_list', video_list);
 
 const port = process.env.PORT;
 app.get('/api/sanity_check', (req: Request, res: Response) => {
     res.send('CSC301 Sanity Check is working');
 });
 
-app.get('/', (req: Request, res: Response) => {
+/* app.get('/', (req: Request, res: Response) => {
     res.send('CSC301 Express Server');
-});
+}); */
 
 if (process.env.NODE_ENV !== 'test') {
     app.listen(port, () => logger.info(`Server started at port ${port}`));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Register from "./pages/register";
 import Login from "./pages/login";
 import HomePage from "./pages/Home";
 import VideoPage from "pages/VideoPage";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 
 const App: React.FC = () => (
     <Router>
