9a4d7188b357251ca4c15afa145b1492568dbf81
JYou015 <jason.you015@gmail.com>
2022-12-01 13:11:40 -0500

    endpoint for private aws for video download

---
+++ api/src/routes/get_video.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+import AWS, { AWSError } from 'aws-sdk'
+import { DeleteObjectOutput } from '@aws-sdk/client-s3';
+import { logger } from '../utils/logger';
+import prisma from '../prisma';
+
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+
+
+const router = express.Router();
+
+const config = {
+    credentials: {
+      accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
+      secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
+    },
+    region:  process.env.AWS_BUCKET_REGION,
+  };
+
+const client = new S3Client(config);
+
+router.post("/", async function(req: Request, res: Response) {
+    console.log(req.body.key)
+    const command = new GetObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: req.body.key,
+      });
+    
+      // await the signed URL and return it
+      res.send(await getSignedUrl(client, command,{ expiresIn: 3600 } ));
+});
+
+
+export default router;
--- frontend/src/pages/VideoPage.tsx
+++ frontend/src/pages/VideoPage.tsx
@@ -14,7 +14,8 @@ const VideoPage = () => {
     if (key === undefined) {
         key = "";
     }
-    const link = "https://tecl-testing.s3.amazonaws.com/" + key.replace(/ /g, "+");
+    // const link = "https://tecl-testing.s3.amazonaws.com/" + key.replace(/ /g, "+");
+    const link = "https://tecl-private-testing.s3.us-east-2.amazonaws.com/pexels-mart-production-8075171.mp4?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEHkaCXVzLWVhc3QtMSJIMEYCIQDa6oop7q6rQNmSGLCFtlJQbgP3%2FPILs3g1c2lblqksjAIhALmC2WSuDPdX52o4poNxhJBHaMO4%2Fi6lE%2BbG952g6FFqKoQDCJL%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMNzQ4MjU5ODU1NTU4IgyvOdFQTOl8Mxmyi8Iq2AKyJJnvbVh9UMW6JYIlFdXR6rJ8VKpar3DC2Havm4Nh6F6JHeoGWtLMAL5a8Zb0nRUA8rdOLPk6GHonsbYDB72SQkpiFG%2F8MFluDW4L2Sbn0t2dIuO8rEFmG9r1%2FRraVg8uYKmhbpIP8vJtI1R3Ylw1W%2BcCx3ol2YIsFOCawKFHxs4xVWrhJ%2F%2BqKqWycMcod9moa9GWdkkikVxYRhyJwbrtept1JqMwg1iizWNt17jJ2ga1%2BPiFsj1B8%2BIagr4m8DmIIvXgZEuGermY4PyQbJG1poZuQh0LUIbWvdEpeiWm%2BgYNIuQAkjfcXR9SOXzmTkNnpZjBkhi%2FWsqaF0JD55DdE7HLY3j30yJCT69fP9USr5mS7UHp0oLNQT7j%2BVkBEtogUv6tq3uwF6Hyy1NnZigS3UUQyCj8g6Hqzqkn6pNOTSPW1J8MRgDiXkNEuQGvexq2Ek16XsTofTD1sqOcBjqyAlZbe8M7KFkUOPNHqhUa%2FMhwf8Vp5pw0f5X5QUEfEOzgJBZWxVyXYFglX%2F571r7Chx21kYFcaHZdF8vhzcCJCcCB11eiHH6yQQEOYxQpnyRL9rbi9XmDnjkG%2Fl7CWZb8d%2BH8GwSmdBwxhMmz1LsrqmOeZGhPX0nuIprpiGDrr6QJCAVKIsz2GvxN7aGcFJLUG2X4xcWuI3sFv6soLhdh%2FFAmoco4Ymryo%2Fvcwan6mfvMligdOaxvlJ3600HqDzrdqZ1Zj4OLCiV6Mr%2BxCNilr0q%2BjCWcIGjTRH4fZz1Vr8mR1oc5EDIlRpX103xjEZt4WtKn1nCLXEte7Q4TBUXgZ0PmMQJniF2ChZw8EtgT44dR5tYS5QjiQf0ohzpQW4A3Sgc5K8aErmoe0q0rmGyqaplyAw%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20221201T164619Z&X-Amz-SignedHeaders=host&X-Amz-Expires=39600&X-Amz-Credential=ASIA24N6DJTDGKLTXSHT%2F20221201%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Signature=961cf93e8db57bb71d5f9c7529e2a0be7010a5cd7d1345a2ef2fb55f598d78ed"
 
     function download(videoLink: string) {
         FileSaver.saveAs(videoLink);
