8f78a28944de7b96903c1379b75dfbbbbcd2fe5d
Kenneth Miura <kenneth.t.miura@gmail.com>
2022-12-04 14:35:39 -0500

    Tags (#83)
    * tag endpoint + migrations
    
    * tags rendering, autocomplete working
    
    Does not handle changing the actual backend
    
    * tags connected to backend
    
    * filtering by tags
    
    * docs

---
+++ api/prisma/migrations/20221202160914_tag_to_video_many_to_many/migration.sql
@@ -0,0 +1,20 @@
+-- DropForeignKey
+ALTER TABLE "Tag" DROP CONSTRAINT "Tag_videoId_fkey";
+
+-- CreateTable
+CREATE TABLE "_TagToVideo" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TagToVideo_AB_unique" ON "_TagToVideo"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TagToVideo_B_index" ON "_TagToVideo"("B");
+
+-- AddForeignKey
+ALTER TABLE "_TagToVideo" ADD CONSTRAINT "_TagToVideo_A_fkey" FOREIGN KEY ("A") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TagToVideo" ADD CONSTRAINT "_TagToVideo_B_fkey" FOREIGN KEY ("B") REFERENCES "Video"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- api/src/routes/video_list.ts
+++ api/src/routes/video_list.ts
@@ -3,6 +3,11 @@ import AWS, { AWSError } from 'aws-sdk'
 import { DeleteObjectOutput } from '@aws-sdk/client-s3';
 import { logger } from '../utils/logger';
 import prisma from '../prisma';
+import { Prisma} from '@prisma/client';
+/** Express router providing video metadata related routes
+ * @module routes/video_lists
+ * @requires express
+ */
 
 const router = express.Router();
 
@@ -22,21 +27,109 @@ router.get('/list', async (req: Request, res: Response) => {
     logger.info("Getting videos from Prisma")
     res.json(await prisma.video.findMany({include: {
         uploader: true,
+        tags: {
+            select: {
+                name: true
+            }
+        }
       },}))
 });
 
+/**
+ * Route that serves a list of all the active tags
+ * @name /tags/
+ * @function
+ * @inner
+ *
+ */
+router.get('/tags', async (req: Request, res: Response) => {
+    logger.info("Getting all tags from Prisma")
+    const results = await prisma.tag.findMany({
+        select: {
+            name: true
+        }
+    });
+    res.json({tags: results.map((result: {name:string}) => result.name)});
+})
 
-// delete videos from s3 and DB 
+/**
+ * Route supporting overwriting video tags
+ * @name /tags/:videoId
+ * @function
+ * @inner
+ *
+ */
+router.patch('/tags/:videoId', async (req: Request, res: Response) => {
+    const videoId = parseInt(req.params.videoId);
+    const tags = req.body.tags;
+    logger.info("Overwriting tags for id: " + videoId + " and tags: " + tags);
+
+    try {
+        await prisma.video.update({
+            where: {
+                id: videoId
+            },
+            data : {
+                tags: {
+                    set: []
+                }
+            }
+        })
+        const updateVideo = await prisma.video.update({
+            where: {
+                id: videoId
+            },
+            data : {
+                tags: {
+                    connectOrCreate:
+                        tags.map((tag:string) => {
+                            return {
+                                where: {name: tag},
+                                create: { name: tag}
+                            }
+                        })
+
+
+
+                }
+            },
+            include: {
+                tags: true
+            }
+        })
+
+        //DELETE tags that no longer have any connections
+        await prisma.tag.deleteMany({
+            where: { videos: { none: {} } },
+        });
+
+
+        res.status(200).json(updateVideo)
+
+    } catch (e){
+        logger.error(e)
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === 'P2025'){
+                res.status(404).send({'message': 'Video not found'});
+            }
+        }
+    }
+
+})
+
+
+
+// delete videos from s3 and DB
 router.post('/delete', async (req: Request, res: Response) => {
     logger.info('Delete endpoint called');
-    const videoId = req.body.fileId; 
-    const bucketName = process.env.AWS_BUCKET_NAME || ''; 
+    const videoId = req.body.fileId;
+    const bucketName = process.env.AWS_BUCKET_NAME || '';
 
     const video = await prisma.video.findUnique({
         where: {
             id: videoId,
         },
-    }); 
+    });
 
     // delete from s3
     if (video != null) {
@@ -65,9 +158,9 @@ router.post('/delete', async (req: Request, res: Response) => {
         where: {
             id: videoId,
         },
-    }); 
+    });
     logger.info('Video deleted from DB');
-    
+
 });
 
 export default router;
